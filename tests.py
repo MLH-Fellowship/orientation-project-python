5945d5d067b36f7c145eb94a37e6ed8ebc87ebb0
Will Russell <will@mlh.io>
2023-05-12 14:17:15 +0100

    Rename test CI

--- tests.py
+++ tests.py
@@ -1,8 +1,6 @@
-import pytest
 from app import app
 
 def test_client():
     response = app.test_client().get('/')
     assert response.status_code == 200
     assert response.json == {"message": "Hello, World!"}
-
